ab5fd8d22bf21a26e0a2882790c3f065a7444dd5
Igor Ivaskiv <windsurfer.ua@gmail.com>
2026-05-21 11:04:47 +0200

    sitename change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://igorivaskiv.github.io',
+  site: 'https://cad-ai.eu',
   vite: {
     plugins: [tailwindcss()]
   }
